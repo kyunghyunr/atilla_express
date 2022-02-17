d60b3946e5ae6072b2cd281a3748e05cfc73bdeb
kyunghyunr <kyunghyunr@gmail.com>
2022-02-17 11:56:22 +0900

    atilla_express_v1.2.2

--- public/javascripts/te5t.xyz.js
+++ public/javascripts/te5t.xyz.js
@@ -7,10 +7,12 @@ const mainMenu = document.querySelector(".mainMenu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   mainMenu.classList.toggle("active");
-})
+});
 
 // hamburgerMenu click after remove
 document.querySelectorAll(".mainMenu_link").forEach(n => n.addEventListener("click", () => {
 	hamburger.classList.remove("active");
 	mainMenu.classList.remove("active");	
-	}))
+	}));
+
+// mobile landscape mode hamburgerMenu delete
